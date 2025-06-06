d22d0f8e091dc915cc4ac43d6932fa2da2fbb281
Franco Lesme <franco.lesme2006@gmail.com>
2025-06-06 13:58:18 -0300

    Update main.c

--- workspace_lpc845/03_servo_control/main.c
+++ workspace_lpc845/03_servo_control/main.c
@@ -1,86 +1,100 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "board.h"
+#include "fsl_sctimer.h"
+#include "fsl_swm.h"
+#include "pin_mux.h"
+#include "fsl_gpio.h"
+#include "fsl_common.h"
+#include "fsl_swm.h"
+#include "fsl_debug_console.h"
 
-// Mapeo de segmentos: A, B, C, D, E, F, G
-//      --B--
-//     |     |
-//     F     A
-//     |     |
-//      --G--
-//     |     |
-//     E     C
-//     |     |
-//      --D--
+#define servo_min_pulse_width 500   // Minimo ancho de pulso en microsegundos
+#define servo_max_pulse_width 1500  // Maximo ancho de pulso en microsegundos
+#define servo_frecuency 50          // Frequencia en Hz (50Hz para servos)
 
-#define LED_A GPIO, 0, 11
-#define LED_B GPIO, 0, 10
-#define LED_C GPIO, 0, 6
-#define LED_D GPIO, 0, 14
-#define LED_E GPIO, 0, 0
-#define LED_F GPIO, 0, 13
-#define LED_G GPIO, 0, 15
+int main(void) {
 
-// Cada bit representa un segmento
-const uint8_t digit_to_segments[10] = {
-    0b00111111, // 0: A B C D E F
-    0b00000101, // 1: A C
-    0b01011011, // 2: A B D E
-    0b01001111, // 3: A B C D G
-    0b01100101, // 4: B C G F
-    0b01101110, // 5: A C D F G
-    0b01111110, // 6: A C D E F G
-    0b00000111, // 7: A B
-    0b01111111, // 8: A B C D E F G
-    0b01101111  // 9: A B C D F G
-};
+    //Habilito Clock de matriz de conmutación (SWM)
+    CLOCK_EnableClock(kCLOCK_Swm);
+    SWM_SetMovablePinSelect(SWM0, kSWM_SCT_OUT0, kSWM_PortPin_P0_18);
+    CLOCK_DisableClock(kCLOCK_Swm);
 
-void display_digit(int digit)
-{
-    if (digit < 0 || digit > 9)
-        return;
-    uint8_t seg = digit_to_segments[digit];
+    // Eligo el clock 
+    uint32_t sctimer_clock = CLOCK_GetFreq(kCLOCK_fro);
+    
+    // Configuro el SCT Timer
+    sctimer_config_t sctimer_config;
+    SCTIMER_GetDefaultConfig(&sctimer_config);
+    SCTIMER_Init(SCT0, &sctimer_config);
 
-    // Encender los segmentos correspondientes
-    GPIO_PinWrite(LED_A, !((seg >> 0) & 1));
-    GPIO_PinWrite(LED_B, !((seg >> 1) & 1));
-    GPIO_PinWrite(LED_C, !((seg >> 2) & 1));
-    GPIO_PinWrite(LED_D, !((seg >> 3) & 1));
-    GPIO_PinWrite(LED_E, !((seg >> 4) & 1));
-    GPIO_PinWrite(LED_F, !((seg >> 5) & 1));
-    GPIO_PinWrite(LED_G, !((seg >> 6) & 1));
+    //Configuracion de PWM
+    sctimer_pwm_signal_param_t pwm_config = {
+        .output = kSCTIMER_Out_0,
+        .level = kSCTIMER_HighTrue,
+        .dutyCyclePercent =  0
+    };
 
-    // Activa solo el dígito izquierdo (A1)
-    GPIO_PinWrite(GPIO, 0, 8, 1); // A1 en bajo (activo)
-    GPIO_PinWrite(GPIO, 0, 9, 1); // A2 en alto (apagado)
-}
+    // Evento para el PWM
+    uint32_t pwm_event = 0;
+    SCTIMER_SetupPwm(SCT0, &pwm_config, servo_frecuency, sctimer_clock, &pwm_event);
 
-int main(void)
-{
-    // Encender los pines correspondientes
-    GPIO_PortInit(GPIO, 0);
-    gpio_pin_config_t out_config = {.pinDirection = kGPIO_DigitalOutput, .outputLogic = 0};
+    // Inicio el SCTIMER
+    SCTIMER_StartTimer(SCT0, kSCTIMER_Counter_U);
 
-    // Inicializo Pin como Salida
-    GPIO_PinInit(GPIO, 0, 11, &out_config);
-    GPIO_PinInit(GPIO, 0, 10, &out_config);
-    GPIO_PinInit(GPIO, 0, 6, &out_config);
-    GPIO_PinInit(GPIO, 0, 14, &out_config);
-    GPIO_PinInit(GPIO, 0, 0, &out_config);
-    GPIO_PinInit(GPIO, 0, 13, &out_config);
-    GPIO_PinInit(GPIO, 0, 15, &out_config);
+    // Configuro el ADC por defecto
+    adc_config_t adc_config;
+    ADC_GetDefaultConfig(&adc_config);
+    ADC_Init(ADC0, &adc_config);
 
-    // Inicializo Pin como Salida
-    GPIO_PinInit(GPIO, 0, 8, &out_config); // A1
-    GPIO_PinInit(GPIO, 0, 9, &out_config); // A2
+    // Prendo el ADC
+    POWER_DisablePD(kPDRUNCFG_PD_ADC0);
+    // Obtengo frecuencia deseada y calibro ADC
+	uint32_t frequency = CLOCK_GetFreq(kCLOCK_Fro) / CLOCK_GetClkDivider(kCLOCK_DivAdcClk);
+	ADC_DoSelfCalibration(ADC0, frequency);
 
-    while (1)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            display_digit(i);
-            for (volatile int d = 0; d < 300000; d++)
-                ; // Retardo simple
-        }
+	// Configuracion del ADC
+	adc_config_t adc_config = {
+		.clockMode = kADC_ClockSynchronousMode,
+		.clockDividerNumber = 1,
+		.enableLowPowerMode = false,
+		.voltageRange = kADC_HighVoltageRange
+	};
+    // Habilito el ADC
+	ADC_Init(ADC0, &adc_config);
+
+	// Configuracion para las conversiones
+	adc_conv_seq_config_t adc_sequence = {
+		.channelMask = 1 << ADC_POT_CH,							// Elijo el canal del potenciometro
+		.triggerMask = 0,										// No hay trigger por hardware
+		.triggerPolarity = kADC_TriggerPolarityPositiveEdge,	// Flanco ascendente
+		.enableSyncBypass = false,								// Sin bypass de trigger
+		.interruptMode = kADC_InterruptForEachSequence			// Interrupciones para cada secuencia
+	};
+	
+    // Configuro y habilito secuencia A
+	ADC_SetConvSeqAConfig(ADC0, &adc_sequence);
+	ADC_EnableConvSeqA(ADC0, true);
+
+	// Habilito interrupcion para el ADC y en el NVIC
+    ADC_EnableInterrupts(ADC0, kADC_ConvSeqAInterruptEnable);
+    NVIC_EnableIRQ(ADC0_SEQA_IRQn);
+
+    // Inicio la conversion del ADC
+    ADC_DoSoftwareTriggerConvSeqA(ADC0);
+
+    while(1){
+        // Leer el ADC
+        adc_result_info_t adc_result;
+        ADC_GetChannelConversionValue(ADC0, 0, &adc_result);
+
+        // Calculo el ancho de pulso con el valor del ADC
+
+
+        // Duty Cycle
+
+
+        // Actualizo el PWM
+        SCTIMER_UpdatePwmDutycycle(SCT0, kSCTIMER_Out_0, duty, kSCTIMER_EdgeAligned, 0);
     }
 }
